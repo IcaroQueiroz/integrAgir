24155808e164250f0eeed507ce28c5847b57beef
IcaroQueiroz <102604400+IcaroQueiroz@users.noreply.github.com>
2023-05-09 18:00:06 -0300

    Melhorias na interface.ui

--- maim.py
+++ maim.py
@@ -1,9 +1,47 @@
 from interface import *
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QWidget , QSizePolicy, QStackedWidget, QLabel, QVBoxLayout
+
+
+from PyQt5 import uic, QtCore
+
 import os
 import sys
 # IMPORT Custom widgets
 from Custom_Widgets.Widgets import *
+
+
+class ErrorNotification(QCustomSlideMenu):
+    def __init__(self, error_message, parent=None):
+        super().__init__(parent)
+        uic.loadUi('interface.ui', self)
+
+        # Configure widget appearance
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        # Find the popupNotificationContantContainer widget and the messageNotification label
+        self.popup_container = self.findChild(QWidget, "popupNotificationContantContainer")
+        self.message_label = self.findChild(QLabel, "messageNotification")
+
+        # Set the error message to the label
+        self.message_label.setText(error_message)
+
+        # Set widget to be hidden by default
+        self.hide()
+
+    def show_as_notification(self):
+        # Calculate widget position
+        parent_geometry = self.parent().geometry()
+        x = parent_geometry.x() + parent_geometry.width() - self.width() - 10
+        y = parent_geometry.y() + parent_geometry.height() - self.height() - 10
+        self.move(x, y)
+
+        # Show widget
+        self.popup_container.show()
+        self.show()
+        self.raise_()
+        self.activateWindow()
+
 
 #--------------------------------------------------------#
 # CLASS MAIN WINDOWS                                    #
@@ -39,21 +77,20 @@ class MainWindow(QMainWindow):
         self.ui.helpBtn.clicked.connect(lambda: self.ui.centerMenuContainer.slideMenu())
         # CLOSE MENU CENTER WIDGET SIZE
         self.ui.closeMenuCenterBtn.clicked.connect(lambda: self.ui.centerMenuContainer.collapseMenu())
-
+        
                 # CLOSE MENU CENTER WIDGET SIZE
-        #self.ui.notificationBtn.clicked.connect(lambda: self.ui.popupNotificationContainer.slideMenu())
-
-"""        self.ui.popupNotificationContainer.customizeQCustomSlideMenu(
-                floatMenu = True, #bool
-                relativeTo = self.ui.centralwidget,
-                position = "center-center",
-                shadowColor = "#000",
-                shadowBlurRadius    = 20,
-                shadowXOffset   = 0,
-                shadowYOffset   = 0)"""
-
+        self.ui.pushButton_6.clicked.connect(lambda: self.show_error_notification('Teste de msg notificação', self))
 
     
+    def show_error_notification(self, error_message, is_permanent=False):
+        notification = ErrorNotification(error_message)
+        notification.setParent(self.ui.centerMenuContainer)  # define QCustomSlideMenu como pai
+        notification.show_as_notification()
+        if not is_permanent:
+            # Esconde a notificação após 5 segundos
+            QtCore.QTimer.singleShot(15000, notification.hide)
+
+   
 
 #--------------------------------------------------------#
 # EXECUÇÃO DA APLICAÇÃO                                  #
